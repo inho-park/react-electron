08a2f3212ac66023cca0a7eb887fb727f26dae46
JunSeong_Park <ttotto0608@naver.com>
2023-05-05 17:34:40 +0900

    adding python source

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import { useState } from "react";
 import VideoRecorder from "./components/recording/VideoRecorder.jsx";
 import AudioRecorder from "./components/recording/AudioRecorder.jsx";
 
-const App = () => {
+// 이거 아마 browser 에서 child_process 를 사용하지 못하게 막는 듯
+// import { exec } from "child_process";
+
+// const onExecution = () => {
+//   var process = exec("python", ["../webmToMp4/WebmToMp4.py", "../video/test4.webm", "../video/test4.mp4"]);
+
+//   process.stdout.on("data", function(data) {
+//     console.log("success");
+//   });
+  
+//   process.stderr.on("data", function(data) {
+//     console.log("false");
+//   });
+// }
+
+function App() {
   let [recordOption, setRecordOption] = useState("video");
 
   const toggleRecordOption = (type) => {
     return () => {
       setRecordOption(type);
     };
-  }
+  };
+
+
 
   return (
-
     <div>
       <h1>React Media Recorder</h1>
       <div className="button-flex">
@@ -25,7 +41,11 @@ const App = () => {
         </button>
       </div>
       <div>
-        {recordOption === "video" ? <VideoRecorder/> : <AudioRecorder/>}
+        {recordOption === "video" ? <VideoRecorder /> : <AudioRecorder />}
+      </div>
+
+      <div>
+        {/* <button onClick={onExecution} type="button">python exe</button> */}
       </div>
     </div>
   );
